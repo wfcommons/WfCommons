26f6a97490c9371c608d91e56d6fcdc9ed96ea74
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-07-31 00:09:14 -0700

    adding distribution fit method

--- workflowhub/trace/trace_analyzer.py
+++ workflowhub/trace/trace_analyzer.py
@@ -16,46 +16,46 @@ from ..utils import best_fit_distribution
 
 
 class TraceAnalyzer:
-	def __init__(self):
-		self.traces: List[Trace] = []
-		self.jobs_summary: JsonDict[str, List[Tuple[float, int]]] = {}
+    def __init__(self):
+        self.traces: List[Trace] = []
+        self.jobs_summary: JsonDict[str, List[Tuple[float, int]]] = {}
 
-	def append_trace(self, trace: Trace):
-		if trace not in self.traces:
-			self.traces.append(trace)
+    def append_trace(self, trace: Trace):
+        if trace not in self.traces:
+            self.traces.append(trace)
 
-	def build_summary(self, jobs_list: List[str]):
-		# build jobs summary
-		for trace in self.traces:
-			for node in trace.workflow.nodes.data():
-				job: Job = node[1]['job']
-				job_name: str = [j for j in jobs_list if j in job.name][0]
-				input_size: int = 0
+    def build_summary(self, jobs_list: List[str]):
+        # build jobs summary
+        for trace in self.traces:
+            for node in trace.workflow.nodes.data():
+                job: Job = node[1]['job']
+                job_name: str = [j for j in jobs_list if j in job.name][0]
+                input_size: int = 0
 
-				for file in job.files:
-					if file.link == "input":
-						input_size += file.size
+                for file in job.files:
+                    if file.link == "input":
+                        input_size += file.size
 
-				if job_name not in self.jobs_summary:
-					self.jobs_summary[job_name] = [(job.runtime, input_size)]
-				else:
-					self.jobs_summary[job_name].append((job.runtime, input_size))
+                if job_name not in self.jobs_summary:
+                    self.jobs_summary[job_name] = [(job.runtime, input_size)]
+                else:
+                    self.jobs_summary[job_name].append((job.runtime, input_size))
 
-		# build traces summary
-		traces_summary: JsonDict[str, Dict[str, Any]] = {}
+        # build traces summary
+        traces_summary: JsonDict[str, Dict[str, Any]] = {}
 
-		for job in self.jobs_summary:
-			runtime_list: List[float] = []
-			inputs_list: List[int] = []
-			for entry in self.jobs_summary[job]:
-				runtime_list.append(entry[0])
-				inputs_list.append(entry[1])
+        for job in self.jobs_summary:
+            runtime_list: List[float] = []
+            inputs_list: List[int] = []
+            for entry in self.jobs_summary[job]:
+                runtime_list.append(entry[0])
+                inputs_list.append(entry[1])
 
-			traces_summary[job] = {
-				"runtime_data": runtime_list,
-				"runtime": best_fit_distribution(runtime_list),
-				"inputs_data": inputs_list,
-				"inputs": best_fit_distribution(inputs_list)
-			}
+            traces_summary[job] = {
+                "runtime_data": runtime_list,
+                "runtime": best_fit_distribution(runtime_list),
+                "inputs_data": inputs_list,
+                "inputs": best_fit_distribution(inputs_list)
+            }
 
-		return traces_summary
+        return traces_summary
--- workflowhub/utils.py
+++ workflowhub/utils.py
@@ -21,61 +21,61 @@ from .types import JsonDict
 
 
 def read_json(trace_filename: str, logger: Optional[Logger] = None) -> JsonDict:
-	"""
-		Reads the json from the file path.
-		returns the json object loaded with json data from the file
-		:param trace_filename:
-		:param logger: the logger uses to output debug information
-		:return: json object
-	"""
-	if logger is None:
-		logger = logging.getLogger("workflowhub")
+    """
+        Reads the json from the file path.
+        returns the json object loaded with json data from the file
+        :param trace_filename:
+        :param logger: the logger uses to output debug information
+        :return: json object
+    """
+    if logger is None:
+        logger = logging.getLogger("workflowhub")
 
-	with open(trace_filename) as data:
-		data_json: JsonDict = json.load(data)
-		logger.info("parsed JSON trace: " + trace_filename)
-		return data_json
+    with open(trace_filename) as data:
+        data_json: JsonDict = json.load(data)
+        logger.info("parsed JSON trace: " + trace_filename)
+        return data_json
 
 
 def best_fit_distribution(data: List[float], logger: Optional[Logger] = None):
-	"""
-		:param data:
-		:param bins:
-		:param logger: the logger uses to output debug information
-		:return:
-	"""
-	if logger is None:
-		logger = logging.getLogger("workflowhub")
+    """
+        :param data:
+        :param bins:
+        :param logger: the logger uses to output debug information
+        :return:
+    """
+    if logger is None:
+        logger = logging.getLogger("workflowhub")
 
-	# get histogram of original data
-	bins = math.ceil(len(data) / 20)
-	y, x = np.histogram(data, bins=bins)
+    # get histogram of original data
+    bins = math.ceil(len(data) / 20)
+    y, x = np.histogram(data, bins=bins)
 
-	# best holders
-	best_distribution = None
-	best_params = (0.0, 1.0)
-	best_sse = np.inf
+    # best holders
+    best_distribution = None
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
 
-	distn_names: List[str] = ['alpha', 'arcsine', 'argus', 'beta', 'chi', 'chi2', 'cosine', 'dgamma', 'dweibull',
-							  'expon', 'fisk', 'gamma', 'gausshyper', 'levy', 'norm', 'pareto', 'rayleigh', 'rdist',
-							  'skewnorm', 'trapz', 'triang', 'uniform', 'wald']
+    distn_names: List[str] = ['alpha', 'arcsine', 'argus', 'beta', 'chi', 'chi2', 'cosine', 'dgamma', 'dweibull',
+                              'expon', 'fisk', 'gamma', 'gausshyper', 'levy', 'norm', 'pareto', 'rayleigh', 'rdist',
+                              'skewnorm', 'trapz', 'triang', 'uniform', 'wald']
 
-	for dist_name in distn_names:
-		# Ignore warnings from data that can't be fit
-		with warnings.catch_warnings():
-			warnings.filterwarnings('ignore')
+    for dist_name in distn_names:
+        # Ignore warnings from data that can't be fit
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore')
 
-			distribution = getattr(scipy.stats, dist_name)
-			params = distribution.fit(y)
+            distribution = getattr(scipy.stats, dist_name)
+            params = distribution.fit(y)
 
-			# calculate fitted PDF and error with fit in distribution
-			pdf = distribution.pdf(x, *params[:-2], loc=params[-2], scale=params[-1])
-			sse = np.sum(np.power(y - pdf[0:bins], 2.0))
+            # calculate fitted PDF and error with fit in distribution
+            pdf = distribution.pdf(x, *params[:-2], loc=params[-2], scale=params[-1])
+            sse = np.sum(np.power(y - pdf[0:bins], 2.0))
 
-			# identify if this distribution is better
-			if best_sse > sse > 0:
-				best_distribution = distribution
-				best_params = params
-				best_sse = sse
+            # identify if this distribution is better
+            if best_sse > sse > 0:
+                best_distribution = distribution
+                best_params = params
+                best_sse = sse
 
-	return best_distribution, best_params
+    return best_distribution, best_params
