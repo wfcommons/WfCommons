744fd9dfd5e0e13ccbf972af54edadd24bc56368
Taina Coleman <tainagdcoleman@gmail.com>
2021-07-08 14:14:14 -0700

    Adds params to the json

--- wfcommons/wfperf/perf.py
+++ wfcommons/wfperf/perf.py
@@ -3,21 +3,56 @@ from typing import Dict
 
 from importlib_metadata import entry_points
 import argparse
+from networkx.algorithms.shortest_paths.unweighted import bidirectional_shortest_path
+import wfcommons
 
 # from wfcommons.wfgen.abstract_recipe import WorkflowRecipe
 from wfcommons.wfchef.recipes import CyclesRecipe, EpigenomicsRecipe, MontageRecipe, BlastRecipe, BwaRecipe, SeismologyRecipe, SoykbRecipe, GenomeRecipe, SrasearchRecipe
 from ..wfchef.chef import get_recipes, get_recipe
 from wfcommons import WorkflowGenerator
+import pathlib
+from typing import List, Dict
+import json
 
-def create_perf_workflow(recipe: str, num_tasks: int, type: str) -> Dict:
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+class NotAllParameters(Exception):
+    pass
+
+def create_perf_workflow(recipe: str, num_tasks: int, benchmark: str, params:List[str]) -> Dict:
     
+    save_dir = pathlib.Path(this_dir.joinpath("benchmark"))
+    save_dir.mkdir(exist_ok=True, parents=True)
+ 
     _recipe = get_recipe(recipe)
     wfname = recipe.split("R")[0]
     generator = WorkflowGenerator(_recipe.from_num_tasks(num_tasks))
     workflow = generator.build_workflow()
-    workflow.write_json(f'{wfname}.json')
+    workflow.write_json(f'{save_dir.joinpath(wfname)}.json')
+
+    with open(f'{save_dir.joinpath(wfname)}.json') as json_file:
+        wf = json.load(json_file)
+    
+    for job in wf["workflow"]["jobs"]:
+        job["params"] = params
+    
+    with open(f'{save_dir.joinpath(wfname)}.json', 'w') as fp:
+        json.dump(wf, fp, indent=4)
+        
+
+    # How do I put executables in each node? And do I have to do this?
+
+    # maybe create perform  ance parameters class on common directory and use object on workflow
+    # Parameters to be added to the JSON: 
+    # IO: file-total size, mode, max_time, max_requests 
+    # Mem: # of threads
 
 
+
+
+    # -----------------DOCUMENTATION---------------------------------
+    # Refer to https://manpages.debian.org/testing/sysbench/sysbench.1.en.html for params format and options
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
@@ -34,7 +69,7 @@ def get_parser() -> argparse.ArgumentParser:
         help="Total number of tasks of workflow."
     )
     parser.add_argument(
-        "-t", "--type",
+        "-b", "--benchmark",
         type=str,
         choices =["CPU", "memory", "IO"],
         help="Total number of tasks of workflow."
@@ -46,11 +81,11 @@ def get_parser() -> argparse.ArgumentParser:
 
 def main():
     parser = get_parser()
-    args = parser.parse_args()
+    args,params = parser.parse_known_args()
     recipe = args.recipe
     num_tasks = args.num_tasks
-    _type = args.type
+    benchmark = args.benchmark
 
-    create_perf_workflow(recipe, num_tasks, _type)
+    create_perf_workflow(recipe, num_tasks, benchmark, params)
 
     
