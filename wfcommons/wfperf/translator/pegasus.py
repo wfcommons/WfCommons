440028b5a0c17e38e20e238f700335bf02d333ec
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-08-25 10:54:27 -0700

    pegasus translator

--- wfcommons/wfperf/translator/pegasus.py
+++ wfcommons/wfperf/translator/pegasus.py
@@ -33,6 +33,39 @@ class PegasusTranslator(Translator):
         """
         Translates a workflow description (WfFormat) into a Pegasus workflow application.
 
-        :param output_file: The name of the output file.
+        :param output_file: The name of the output file (e.g., workflow.py).
         :type output_file: str
         """
+        script = "import os\n" \
+                 "from Pegasus.api import *\n\n\n" \
+                 "def which(file):\n" \
+                 "    for path in os.environ['PATH'].split(os.pathsep):\n" \
+                 "        if os.path.exists(os.path.join(path, file)):\n" \
+                 "            return os.path.join(path, file)\n" \
+                 "    return None\n\n\n"
+
+        # overall workflow
+        script += "wf = Workflow('{}', infer_dependencies=True)\n\n".format(self.instance.instance['name'])
+
+        # transformation catalog
+        script += "tc = TransformationCatalog()\n" \
+                  "full_path = which('sys_test.py')\n" \
+                  "if full_path is None:\n" \
+                  "    raise RuntimeError('sys_test.py is not in the $PATH')\n" \
+                  "base_dir = os.path.dirname(full_path)\n" \
+                  "transformation = Transformation('sys_test.py', site='local',\n" \
+                  "                                pfn=os.path.join(base_dir, 'sys_test.py'), is_stageable=True)\n" \
+                  "transformation.add_env(PATH='/usr/bin:/bin:.')\n" \
+                  "tc.add_transformations(transformation)\n" \
+                  "wf.add_transformation_catalog(tc)\n\n"
+
+        # replica catalog
+        script += "rc = ReplicaCatalog()\n" \
+                  "wf.add_replica_catalog(rc)\n\n"
+
+        # write out the workflow
+        script += "wf.write('{}-benchmark-workflow.yml')\n".format(self.instance.instance['name'])
+
+        # write script to file
+        with open(output_file, 'w') as out:
+            out.write(script)
