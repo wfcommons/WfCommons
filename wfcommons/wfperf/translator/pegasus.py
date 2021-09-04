737194cac9516873e809dd6998747aca8bbda73f
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-09-04 15:58:04 -0700

    temporary pegasus changes

--- wfcommons/wfperf/translator/pegasus.py
+++ wfcommons/wfperf/translator/pegasus.py
@@ -8,8 +8,10 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
+import uuid
+
 from logging import Logger
-from typing import List, Optional
+from typing import Optional
 
 from .abstract_translator import Translator
 
@@ -51,6 +53,7 @@ class PegasusTranslator(Translator):
         self.script += "wf = Workflow('{}', infer_dependencies=True)\n" \
                        "tc = TransformationCatalog()\n" \
                        "rc = ReplicaCatalog()\n\n".format(self.instance.name)
+        self.script += "task_output_files = {}\n\n"
 
         # transformation catalog
         self.script += "full_path = which('sys_test.py')\n" \
@@ -61,10 +64,19 @@ class PegasusTranslator(Translator):
                        "                                pfn=os.path.join(base_dir, 'sys_test.py'),\n" \
                        "                                is_stageable=True)\n" \
                        "transformation.add_env(PATH='/usr/bin:/bin:.')\n" \
+                       "transformation.add_profiles(Namespace.CONDOR, 'request_disk', '10')\n" \
                        "tc.add_transformations(transformation)\n\n"
         # adding tasks
         for task_name in self.parent_task_names:
             self._add_task(task_name)
+            # input file
+            input_file = str(uuid.uuid4())
+            self.script += "in_file_{} = File('{}')\n".format(self.task_counter, input_file)
+            self.script += "rc.add_replica('local', '{}', 'file://' + os.getcwd() + '/data/{}')\n".format(input_file,
+                                                                                                          input_file)
+            self.script += "{}.add_inputs(in_file_{})\n".format(self.tasks_map[task_name], self.task_counter)
+            self.script += "print('{}')\n".format(input_file)
+
         self.script += "\n"
 
         # write out the workflow
@@ -88,10 +100,18 @@ class PegasusTranslator(Translator):
         if task_name not in self.parsed_tasks:
             task = self.tasks[task_name]
             job_name = "job_{}".format(self.task_counter)
+            out_file = str(uuid.uuid4())
             self.script += "{} = Job(transformation=transformation, _id='{}')\n".format(job_name, task_name)
+            task.args.insert(0, task_name.split("_")[0])
+            task.args.append("--out={}".format(out_file))
             self.script += "{}.add_args({})\n".format(job_name, ", ".join(f"'{a}'" for a in task.args))
-            self.script += "wf.add_jobs({})\n".format(job_name)
-            self.script += "\n"
+
+            self.script += "out_file_{} = File('{}')\n".format(self.task_counter, out_file)
+            self.script += "task_output_files['{}'] = out_file_{}\n".format(job_name, self.task_counter)
+            self.script += "{}.add_outputs(out_file_{}, stage_out=False, register_replica=False)\n".format(
+                job_name, self.task_counter)
+
+            self.script += "wf.add_jobs({})\n\n".format(job_name)
             self.task_counter += 1
             self.parsed_tasks.append(task_name)
             self.tasks_map[task_name] = job_name
@@ -102,4 +122,5 @@ class PegasusTranslator(Translator):
                         self._add_task(child_task_name, job_name)
 
         if parent_task:
+            self.script += "{}.add_inputs(task_output_files['{}'])\n".format(self.tasks_map[task_name], parent_task)
             self.script += "wf.add_dependency({}, parents=[{}])\n\n".format(self.tasks_map[task_name], parent_task)
