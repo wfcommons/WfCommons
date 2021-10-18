5968cb12b1b03547b1febc9b767b604c31b24f2a
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-10-18 10:21:22 -0700

    adding dependency to transformations

--- wfcommons/wfperf/translator/pegasus.py
+++ wfcommons/wfperf/translator/pegasus.py
@@ -62,7 +62,7 @@ class PegasusTranslator(Translator):
 
         # cpu-benchmark
         self.script += "t_cpu_benchmark = Transformation('cpu-benchmark', site='local',\n" \
-                       "pfn='./cpu-benchmark', is_stageable=True)\n" \
+                       "pfn=os.getcwd() + '/cpu-benchmark', is_stageable=True)\n" \
                        "tc.add_transformations(t_cpu_benchmark)\n\n"
 
         # tasks' programs
