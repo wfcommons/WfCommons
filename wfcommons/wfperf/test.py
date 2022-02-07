e57453bff57b026d58dfc05e25a6c9effc0c52d7
Taina Coleman <tainagdcoleman@gmail.com>
2022-02-07 19:32:11 +0000

    Organize test.py

--- wfcommons/wfperf/test.py
+++ wfcommons/wfperf/test.py
@@ -3,6 +3,7 @@ from wfcommons.wfperf.perf import WorkflowBenchmark
 import pathlib
 import glob
 import os
+import argparse
 
 this_dir = pathlib.Path(__file__).parent.resolve()
 # Rememeber to run as PYTHONUNBUFFERED=1 python test.py 
@@ -14,13 +15,28 @@ def cleanup_sys_files() -> None:
     all_files = input_files + output_files
     for t in all_files:
         os.remove(t)
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datafootprint", help="Runs with datafootprint.")
+    parser.add_argument("--input-file", help="Runs from input-file.")
+   
+    return parser
+
 def main():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
     bench = WorkflowBenchmark(BlastRecipe, 100)
-    # bench.create_benchmark(this_dir, 0.5, data=10)
-    # bench.generate_input_file(this_dir.joinpath("test.json"))
-    # bench.create_benchmark_from_input_file(this_dir, this_dir.joinpath("test.json"), lock_files_folder=this_dir.joinpath("tmp"))
-    # bench.run(pathlib.Path("/workspace/wfcommons/wfcommons/wfperf/Blast-Benchmark-100.json"),this_dir)
-    cleanup_sys_files()
+    if args.datafootprint:
+        bench.create_benchmark(this_dir, 0.5, data=10)
+    
+    if args.input_file:
+        bench.generate_input_file(this_dir.joinpath("test.json"))
+        bench.create_benchmark_from_input_file(this_dir, this_dir.joinpath("test.json"), lock_files_folder=this_dir.joinpath("tmp"))
+    
+    bench.run(pathlib.Path("/workspace/wfcommons/wfcommons/wfperf/Blast-Benchmark-100.json"),this_dir)
+    # cleanup_sys_files()
 
 if __name__ == "__main__":
     main()  
