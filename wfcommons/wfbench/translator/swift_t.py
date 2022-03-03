b74f25ac8f8939c432c9b823f4579c890c4bbbff
Rafael Ferreira da Silva <silvarf@ornl.gov>
2022-03-02 20:26:44 -0500

    fixing issue on swift/t translator code

--- wfcommons/wfbench/translator/swift_t.py
+++ wfcommons/wfbench/translator/swift_t.py
@@ -275,26 +275,16 @@ class SwiftTTranslator(Translator):
         self.script += f"string {category}_in = \"{in_str}\";\n"
 
         if num_tasks > 1:
-            counter = 0
-            limit = 2000
-            index = 0
-            while num_tasks - counter > limit or counter < num_tasks:
-                self.script += f"int {category}__out_{index}[];\n" \
-                    f"foreach i in [0:{min(limit, num_tasks - counter) - 1}] {{\n" \
-                    f"  int index; index = {counter} + i;\n" \
-                    f"  string of = sprintf(\"{category}_%i_output.txt\", index);\n" \
-                    f"  string cmd_{self.cmd_counter} = sprintf(command, \"{category}\", {args});\n" \
-                    f"  string co_{self.cmd_counter} = python(cmd_{self.cmd_counter});\n" \
-                    f"  string of_{self.cmd_counter} = sprintf(\"0%s\", co_{self.cmd_counter});\n" \
-                    f"  {category}__out_{index}[i] = string2int(of_{self.cmd_counter});\n" \
-                    "}\n\n"
-                counter += limit
-                index += 1
+            self.script += f"int {category}__out_l[]; \n" \
+                f"foreach i in [0:{num_tasks - 1}] {{\n" \
+                f"  string of = sprintf(\"{category}_%i_output.txt\", index);\n" \
+                f"  string cmd_{self.cmd_counter} = sprintf(command, \"{category}\", {args});\n" \
+                f"  string co_{self.cmd_counter} = python(cmd_{self.cmd_counter});\n" \
+                f"  string of_{self.cmd_counter} = sprintf(\"0%s\", co_{self.cmd_counter});\n" \
+                f"  {category}__out[i] = string2int(of_{self.cmd_counter});\n" \
+                "}\n" \
+                f"{category}__out[0] = {category}__out_l[{num_tasks - 1}];\n\n"
             
-            for i in range(0, index):
-                self.script += f"{category}__out[{i}] = {category}__out_{i}[0];\n"
-            self.script += "\n"
-
         else:
             args = args.replace(
                 ", of", f", \"{category}_0_output.txt\"").replace("[i]", "[0]")
