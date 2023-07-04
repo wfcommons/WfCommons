3bafbe168e05bc8e0cce9b7762070cd2c5688c34
Rafael Ferreira da Silva <silvarf@ornl.gov>
2023-07-04 18:05:04 -0400

    removing Python 3.7 and adding 3.11

--- setup.py
+++ setup.py
@@ -64,10 +64,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
@@ -75,7 +75,7 @@ setup(
         'Topic :: Documentation :: Sphinx',
         'Topic :: System :: Distributed Computing'
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     data_files=[
         ('bin', ['bin/cpu-benchmark'])
     ],
