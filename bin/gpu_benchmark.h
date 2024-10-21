8c5d7312a49489055f5aef2fe62b0c3b9876e023
Taina Coleman <tainagdcoleman@gmail.com>
2024-10-21 13:25:41 -0700

    Fixes gpu code

--- bin/gpu_benchmark.h
+++ bin/gpu_benchmark.h
@@ -3,7 +3,8 @@
 
 #include <cuda_runtime.h>
 
-void runBenchmark(int max_work, int runtime_in_seconds);
+void runBenchmark(int max_work);
+void runBenchmarkTime(int max_work, int runtime_in_seconds);
 
 #endif // GPU_BENCHMARK_H
 
