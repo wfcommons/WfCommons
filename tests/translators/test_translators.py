d91c037b2e8dd0ca914046d418b24d4727701aab
Henri Casanova <henric@hawaii.edu>
2025-08-05 11:20:43 -1000

    disabling Nextflow test

--- tests/translators/test_translators.py
+++ tests/translators/test_translators.py
@@ -178,7 +178,7 @@ class TestTranslators:
         assert(num_completed_tasks == num_tasks)
 
     @pytest.mark.unit
-    # @pytest.mark.skip(reason="tmp")
+    @pytest.mark.skip(reason="tmp")
     def test_nextflow_translator(self) -> None:
 
         # Create workflow benchmark
