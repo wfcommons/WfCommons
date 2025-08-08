919a9741e2c3b074a0351f57b0209b129c1c05cd
Henri Casanova <henric@hawaii.edu>
2025-08-07 22:09:55 -1000

    Added a TaskVine translator test Fixed some bugs in the TaskVine translator

--- tests/translators/test_translators.py
+++ tests/translators/test_translators.py
@@ -21,7 +21,7 @@ import json
 import time
 
 from wfcommons import BlastRecipe
-from wfcommons.wfbench import WorkflowBenchmark
+from wfcommons.wfbench import WorkflowBenchmark, TaskVineTranslator
 from wfcommons.wfbench import DaskTranslator
 from wfcommons.wfbench import ParslTranslator
 from wfcommons.wfbench import NextflowTranslator
@@ -29,7 +29,7 @@ from wfcommons.wfbench import AirflowTranslator
 from wfcommons.wfbench import BashTranslator
 
 
-def _start_docker_container(backend, mounted_dir, working_dir, bin_dir):
+def _start_docker_container(backend, mounted_dir, working_dir, bin_dir, command=["sleep", "infinity"]):
     # Pulling the Docker image
     client = docker.from_env()
     image_name = f"wfcommons/wfcommons-testing-{backend}"
@@ -45,7 +45,7 @@ def _start_docker_container(backend, mounted_dir, working_dir, bin_dir):
     sys.stderr.write("Starting Docker container...\n")
     container = client.containers.run(
         image_name,
-        command=["sleep", "infinity"],
+        command=command,
         volumes={mounted_dir: {'bind': mounted_dir, 'mode': 'rw'}},
         working_dir=working_dir,
         tty=True,
@@ -101,6 +101,32 @@ def _create_workflow_benchmark():
         num_tasks = len(generated_json["workflow"]["specification"]["tasks"])
     return benchmark, num_tasks
 
+#############################################################################
+## Methods to perform additional setup required by the backend
+#############################################################################
+
+def noop(container):
+    pass
+
+def _additional_setup_taskvine(container):
+    # Create the poncho package
+    exit_code, output = container.exec_run(cmd=["bash", "-c",
+                                                "source ~/conda/etc/profile.d/conda.sh && conda activate && poncho_package_create taskvine_poncho.json taskvine_poncho.tar.gz"],
+                                           stdout=True, stderr=True)
+    # Start a vine worker in the background
+    exit_code, output = container.exec_run(
+        cmd=["bash", "-c", "source ~/conda/etc/profile.d/conda.sh && conda activate && vine_worker localhost 9123"],
+        detach=True, stdout=True, stderr=True)
+
+
+additional_setup_methods = {
+    "dask": noop,
+    "parsl": noop,
+    "nextflow": noop,
+    "airflow": noop,
+    "bash": noop,
+    "taskvine": _additional_setup_taskvine,
+}
 
 #############################################################################
 ## Methods to run the various workflows, kill the container, and check sanity
@@ -137,6 +163,7 @@ def run_workflow_nextflow(container, num_tasks, str_dirpath):
 
 def run_workflow_airflow(container, num_tasks, str_dirpath):
     # Run the workflow! (use a specific working directory)
+    # TODO: Remove the hardcoded Blast-Benchmark as it's ugly
     exit_code, output = container.exec_run(cmd=["sh", "-c", "cd /home/wfcommons/ && sudo /bin/bash /run_a_workflow.sh Blast-Benchmark"],
                                            stdout=True,
                                            stderr=True)
@@ -155,12 +182,24 @@ def run_workflow_bash(container, num_tasks, str_dirpath):
     assert (exit_code == 0)
     assert (output.decode().count("completed") == num_tasks)
 
+def run_workflow_taskvine(container, num_tasks, str_dirpath):
+
+    # Run the workflow!
+    exit_code, output = container.exec_run(cmd=["bash", "-c", "source ~/conda/etc/profile.d/conda.sh && conda activate && python3 ./taskvine_workflow.py"], stdout=True, stderr=True)
+    # Kill the container
+    container.remove(force=True)
+    # # Check sanity
+    assert (exit_code == 0)
+    assert (output.decode().count("completed") == num_tasks)
+
+
 run_workflow_methods = {
     "dask": run_workflow_dask,
     "parsl": run_workflow_parsl,
     "nextflow": run_workflow_nextflow,
     "airflow": run_workflow_airflow,
     "bash": run_workflow_bash,
+    "taskvine": run_workflow_taskvine,
 }
 
 translator_classes = {
@@ -169,15 +208,9 @@ translator_classes = {
     "nextflow": NextflowTranslator,
     "airflow": AirflowTranslator,
     "bash": BashTranslator,
+    "taskvine": TaskVineTranslator,
 }
 
-container_exec_workflow_dir = {
-    "dask": DaskTranslator,
-    "parsl": ParslTranslator,
-    "nextflow": NextflowTranslator,
-    "airflow": AirflowTranslator,
-    "bash": BashTranslator,
-}
 
 class TestTranslators:
 
@@ -189,10 +222,11 @@ class TestTranslators:
             "nextflow",
             "airflow",
             "bash",
+            "taskvine",
         ])
     @pytest.mark.unit
     # @pytest.mark.skip(reason="tmp")
-    def test_bash_translator(self, backend) -> None:
+    def test_translator(self, backend) -> None:
         # Create workflow benchmark
         benchmark, num_tasks = _create_workflow_benchmark()
 
@@ -207,11 +241,14 @@ class TestTranslators:
         translator = translator_classes[backend](benchmark.workflow)
         translator.translate(output_folder=dirpath)
 
-        # Starting the Docker container (Using the parsl container, which is not necessary)
+        # Start the Docker container
         sys.stderr.write("Starting Docker container...\n")
         container = _start_docker_container(backend, str_dirpath, str_dirpath, str_dirpath + "bin/")
 
-        # Running the workflow
+        # Do whatever necessary setup
+        additional_setup_methods[backend](container)
+
+        # Run the workflow
         sys.stderr.write("Running workflow...\n")
         start_time = time.time()
         run_workflow_methods[backend](container, num_tasks, str_dirpath)
