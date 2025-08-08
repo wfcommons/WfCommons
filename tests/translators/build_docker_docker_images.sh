b38bfc2e4dd88b8b479e1b6d5f12c98d11e8e956
Henri Casanova <henric@hawaii.edu>
2025-08-08 08:24:20 -1000

    Added a CWL translator tester

--- tests/translators/build_docker_docker_images.sh
+++ tests/translators/build_docker_docker_images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for backend in "dask" "parsl" "nextflow" "airflow" "bash" "taskvine"; do
+for backend in "dask" "parsl" "nextflow" "airflow" "bash" "taskvine" "cwl"; do
   echo "Building $backend Docker image..."
   docker build --platform linux/amd64 -t wfcommons/wfcommons-testing-$backend -f Dockerfile.$backend .
 done
