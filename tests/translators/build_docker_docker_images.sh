c3e830e78b72d97d8376525bdd16524df29bbc36
Henri Casanova <henric@hawaii.edu>
2025-08-13 16:29:56 -1000

    Script update

--- tests/translators/build_docker_docker_images.sh
+++ tests/translators/build_docker_docker_images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for backend in "dask" "parsl" "nextflow" "airflow" "bash" "taskvine" "cwl"; do
+for backend in "dask" "parsl" "nextflow" "airflow" "bash" "taskvine" "cwl" "pegasus"; do
   echo "Building $backend Docker image..."
   docker build --platform linux/amd64 -t wfcommons/wfcommons-testing-$backend -f Dockerfile.$backend .
 done
