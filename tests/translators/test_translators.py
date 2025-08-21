6a3a79981c91014e8a27ebff256722c4a845ced1
Henri Casanova <henric@hawaii.edu>
2025-08-20 16:44:39 -1000

    Added (not validate) swift/t Dockerfile

--- tests/translators/test_translators.py
+++ tests/translators/test_translators.py
@@ -28,70 +28,10 @@ from wfcommons.wfbench import BashTranslator
 from wfcommons.wfbench import TaskVineTranslator
 from wfcommons.wfbench import CWLTranslator
 from wfcommons.wfbench import PegasusTranslator
+from wfcommons.wfbench import SwiftTTranslator
 
 from wfcommons.wfinstances import PegasusLogsParser
 
-# def _start_docker_container(backend, mounted_dir, working_dir, bin_dir, command=None):
-#     if command is None:
-#         command = ["sleep", "infinity"]
-#     # Pulling the Docker image
-#     client = docker.from_env()
-#     image_name = f"wfcommons/wfcommons-testing-{backend}"
-#
-#     try:
-#         image = client.images.get(image_name)
-#         sys.stderr.write(f"Image '{image_name}' is available locally\n")
-#     except ImageNotFound:
-#         sys.stderr.write(f"Pulling image '{image_name}'...\n")
-#         client.images.pull(image_name)
-#
-#     # Launch the docker container to actually run the translated workflow
-#     sys.stderr.write("Starting Docker container...\n")
-#     container = client.containers.run(
-#         image=image_name,
-#         command=command,
-#         volumes={mounted_dir: {'bind': mounted_dir, 'mode': 'rw'}},
-#         working_dir=working_dir,
-#         tty=True,
-#         detach=True
-#     )
-#
-#     # Installing WfCommons on container
-#     _install_WfCommons_on_container(container)
-#
-#     # Copy over the wfbench and cpu-benchmark executables to where they should go on the container
-#     if bin_dir:
-#         exit_code, output = container.exec_run(["sh", "-c", "sudo cp -f `which wfbench` " + bin_dir],
-#                                                stdout=True, stderr=True)
-#         exit_code, output = container.exec_run(["sh", "-c", "sudo cp -f `which cpu-benchmark` " + bin_dir],
-#                                                stdout=True, stderr=True)
-#
-#     return container
-
-# def _make_tarfile_of_wfcommons():
-#     source_dir = os.getcwd() # This assumes the testing is run from the root
-#     tar_stream = io.BytesIO()
-#     with tarfile.open(fileobj=tar_stream, mode='w') as tar:
-#         tar.add(source_dir, arcname=os.path.basename(source_dir))
-#     tar_stream.seek(0)
-#     return tar_stream
-#
-# def _install_WfCommons_on_container(container):
-#     # sys.stderr.write("Installing WfCommons on the container...\n")
-#     # Copy the WfCommons code to it (removing stuff that should be removed)
-#     target_path = '/tmp/'  # inside container
-#     tar_data = _make_tarfile_of_wfcommons()
-#     container.put_archive(target_path, tar_data)
-#     exit_code, output = container.exec_run("sudo /bin/rm -rf /tmp/WfCommons/build/", stdout=True, stderr=True)
-#     exit_code, output = container.exec_run("sudo /bin/rm -rf /tmp/WfCommons/*.egg-info/", stdout=True, stderr=True)
-#     exit_code, output = container.exec_run("sudo /bin/rm -rf /tmp/WfCommons/bin/cpu-benchmark.o", stdout=True,
-#                                            stderr=True)
-#     exit_code, output = container.exec_run("sudo /bin/rm -rf /tmp/WfCommons/bin/cpu-benchmark", stdout=True,
-#                                            stderr=True)
-#
-#     # Install WfCommons on the container (to install wfbench and cpu-benchmark really)
-#     exit_code, output = container.exec_run("sudo python3 -m pip install . --break-system-packages",
-#                                            workdir="/tmp/WfCommons", stdout=True, stderr=True)
 
 def _create_workflow_benchmark():
     # Create a workflow benchmark object to generate specifications based on a recipe (in /tmp/, whatever)
@@ -142,6 +82,7 @@ additional_setup_methods = {
     "taskvine": _additional_setup_taskvine,
     "cwl": noop,
     "pegasus": _additional_setup_pegasus,
+    "swiftt": noop,
 }
 
 #############################################################################
@@ -229,6 +170,16 @@ def run_workflow_pegasus(container, num_tasks, str_dirpath):
     assert(exit_code == 0)
     assert("success" in output.decode())
 
+def run_workflow_swiftt(container, num_tasks, str_dirpath):
+    # Run the workflow!
+    # exit_code, output = container.exec_run(cmd="bash /home/wfcommons/run_workflow.sh", stdout=True, stderr=True)
+    # # Kill the container
+    # container.remove(force=True)
+    # # Check sanity
+    # assert(exit_code == 0)
+    # assert("success" in output.decode())
+    pass
+
 run_workflow_methods = {
     "dask": run_workflow_dask,
     "parsl": run_workflow_parsl,
@@ -238,6 +189,7 @@ run_workflow_methods = {
     "taskvine": run_workflow_taskvine,
     "cwl": run_workflow_cwl,
     "pegasus": run_workflow_pegasus,
+    "swiftt": run_workflow_swiftt,
 }
 
 translator_classes = {
@@ -249,6 +201,7 @@ translator_classes = {
     "taskvine": TaskVineTranslator,
     "cwl": CWLTranslator,
     "pegasus": PegasusTranslator,
+    "swiftt": SwiftTTranslator,
 }
 
 logs_parser_classes = {
@@ -261,14 +214,15 @@ class TestTranslators:
     @pytest.mark.parametrize(
         "backend",
         [
-            "dask",
-            "parsl",
-            "nextflow",
-            "airflow",
-            "bash",
-            "taskvine",
-            "cwl",
-            "pegasus",
+            # "dask",
+            # "parsl",
+            # "nextflow",
+            # "airflow",
+            # "bash",
+            # "taskvine",
+            # "cwl",
+            # "pegasus",
+            "swiftt",
         ])
     @pytest.mark.unit
     # @pytest.mark.skip(reason="tmp")
