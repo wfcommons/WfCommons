5aefaba4aed1e4b5d80e42e684342a375b77d013
Steven Hahn <hahnse@ornl.gov>
2026-07-13 17:10:59 -0400

    hipify kernel
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>

---
+++ bin/hip/zero_one.h
@@ -0,0 +1,11 @@
+#include "hip/hip_runtime.h"
+#ifndef __ZERO_ONE_CUH__
+#define __ZERO_ONE_CUH__
+
+__global__ void stress_vm_zero(unsigned long long int *buf,
+                               unsigned long long int *count,
+                               const size_t sz);
+__global__ void stress_vm_one(unsigned long long int *buf,
+                              unsigned long long int *count,
+                              const size_t sz);
+#endif
