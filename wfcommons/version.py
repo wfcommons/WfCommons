f000f3dff68b0d089fc31e38c856641315f753b6
rafaelfsilva <silvarf@ornl.gov>
2026-07-26 20:47:47 -0400

    preparing repo for v1.6-dev

--- wfcommons/version.py
+++ wfcommons/version.py
@@ -8,5 +8,5 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-__version__ = "1.5"
+__version__ = "1.6-dev"
 __schema_version__ = "1.5"
